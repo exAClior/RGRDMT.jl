6f712fbb7208c569448bf833631dcf867796fe7f
Yusheng Zhao <yushengzhao2020@outlook.com>
2024-06-26 17:20:26 +0800

    fix examples CI

--- example/example.jl
+++ example/example.jl
@@ -1,6 +1,7 @@
+using Pkg; Pkg.activate(dirname(@__FILE__))
 using Test, RGRDMT, Yao
 using MPSKit, MPSKitModels, TensorKit
-using SCS, MosekTools, Dualization
+using SCS, MosekTools 
 using Random
 using JuMP
 using DelimitedFiles
@@ -41,10 +42,10 @@ end
 
 function booda_dual()
     h = mat(-kron(Z, Z) / 4 - 1 / 8 * kron(X, I2) - 1 / 8 * kron(I2, X))
-    main_dual(h, 3:8, -1 / π, "data/etfi_dual.csv", "data/ntfi_dual.csv", SCS.Optimizer)
+    main_dual(h, 3:6, -1 / π, "etfi.csv", "ennn.csv", SCS.Optimizer)
 end
 
-# booda_dual() # this is currently the best option, could also try on Linux machinehttps://discourse.julialang.org/t/multi-core-parallel-support-for-jump-supported-solvers/112392/3 
+booda_dual() # this is currently the best option, could also try on Linux machinehttps://discourse.julialang.org/t/multi-core-parallel-support-for-jump-supported-solvers/112392/3 
 
 function booda2(D)
     h = mat(kron(Z, Z) / 4 + 1 / 8 * kron(X, I2) + 1 / 8 * kron(I2, X))
@@ -56,26 +57,24 @@ function booda2(D)
     k0 = Int(floor(2 * log(D) / log(d)) + 1)
     V0, L, R = CGmapping_from_AL(AL, k0, k0 + 2)
 
-    main2(h, D, -1 / π, vcat(k0+2:2:k0+10, k0+12:10:70), V0, L, R, "data/etfi$D.csv", "data/ntfi$D.csv", MosekTools.Optimizer)
+    # main2(h, D, -1 / π, vcat(k0+2:2:k0+10, k0+12:10:70), V0, L, R, "data/etfi$D.csv", "data/ntfi$D.csv", MosekTools.Optimizer)
+    main2(h, D, -1 / π, k0+2:2:k0+10, V0, L, R, "etfi$D.csv", "ntfi$D.csv", MosekTools.Optimizer)
 end
 
-# booda2(2)
-# booda2(3)
-# booda2(4)
-# booda2(5)
-# booda2(6)
+booda2(2)
 
 function dooda_dual()
     h = mat(-kron(X, X) - kron(Y, Y) + kron(Z, Z)) / 4
-    main_dual(h, 3:12, 1 / 4 - log(2), "data/exxx.csv", "data/nxxx.csv", SCS.Optimizer)
+    main_dual(h, 3:6, 1 / 4 - log(2), "exxx.csv", "nxxx.csv", SCS.Optimizer)
 end
 
-# dooda_dual()
+dooda_dual()
 
 function dooda2(D)
     h = mat(-kron(X, X) - kron(Y, Y) + kron(Z, Z)) / 4
     d = 2
     filename = "data/HeisenbergSpinHalf_SubLatticeRotation_Data.mat"
+    filename = joinpath(dirname(dirname(@__FILE__)), filename)
     ψ, upperBd = load_MPS(filename, D)
     AL = ψ.AL[]
 
@@ -85,12 +84,7 @@ function dooda2(D)
     L = Complex.(L)
     R = Complex.(R)
 
-    main2(h, D, 1 / 4 - log(2), 8:2:64, V0, L, R, "data/exxx$D.csv", "data/nxxx$D.csv", MosekTools.Optimizer)
+    main2(h, D, 1 / 4 - log(2), 8:2:64, V0, L, R, "exxx$D.csv", "nxxx$D.csv", MosekTools.Optimizer)
 end
 
-# dooda2(2)
-# dooda2(3)
-# dooda2(4)
-# dooda2(5)
-# dooda2(6)
-# dooda2(7)
+dooda2(2)
